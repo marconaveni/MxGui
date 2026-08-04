493e73a02f8f44e0c4e3ae5f3e9d986a45c5d4f1
Marco Naveni <marconaveni.mco@hotmail.com>
2026-08-04 20:41:10 -0300

    [project] added version

---
+++ mxgui_config.hpp
@@ -0,0 +1,10 @@
+
+
+#define FORCE_DEBUG 0                    // Force debug
+#define MX_FONT_NOTO_ID "notosans"       // Font default ID
+#define MX_FONT_AWESOME_ID "fontawesome" // Font awesome ID
+#define MX_DRAG_OFFSET 4                 // Extra area px rect drags
+#define MX_TEXT_LINE_SPACING 0.0f        // Config \n space in texts
+#define MX_BAR_SIZE 6                    // Bar size scroll
+#define MX_FONT_AWESOME 1                // Enable font_awesome (0 - disabled | 1 enabled)
+#define MX_SUPPRESS_WARNINGS 1           // Supress Warnings    (0 - disabled | 1 enabled)
